fce27ffe6398c183ac83cfd3d8df6b4fc1022925
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-01-29 07:42:32 +0200

    project-content updated, and reading the markdown implemented

--- components/Projects/project-detail/project-content.js
+++ components/Projects/project-detail/project-content.js
@@ -1,20 +1,56 @@
 import ReactMarkdown from 'react-markdown';
-
-import ProjectHeader from "./project-header"
-import classes from './project-content.module.css'
-
-
+import ProjectHeader from "./project-header";
+import classes from './project-content.module.css';
+import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 function ProjectContent(props) {
-    const {project} = props
-    const imagePath = `/images/projects/${project.slug}/${project.image}`
+    const { project } = props;
+    const imagePath = `/images/projects/${project.slug}/${project.image}`;
+
+    const customRenderers = {
+        p(paragraph) {
+            const { node } = paragraph;
+
+            if (node.children[0].tagName === 'img') {
+                const image = node.children[0];
+                const imageAlt = image.alt || "Image pf the project"; // Fallback alt text
+                return (
+                    <div className={classes.image}>
+                        <Image
+                            src={`/images/projects/${project.slug}/${image.properties.src}`}
+                            alt={imageAlt}
+                            width={600}
+                            height={300}
+                        />
+                    </div>
+                );
+            }
+
+            return <p>{paragraph.children}</p>;
+        },
+
+        code(code) {
+            const { className, children } = code;
+            const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+            return (
+                <SyntaxHighlighter
+                    style={atomDark}
+                    language={language}
+                    children={children}
+                />
+            );
+        },
+        // Add other custom renderers if needed
+    };
 
     return (
         <article className={classes.content}>
             <ProjectHeader title={project.title} image={imagePath} />
-            <ReactMarkdown>{project.content}</ReactMarkdown>
+            <ReactMarkdown components={customRenderers}>{project.content}</ReactMarkdown>
         </article>
-    )
+    );
 }
 
-export default ProjectContent
+export default ProjectContent;
