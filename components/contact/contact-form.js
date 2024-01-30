de76ce30dbe7c589cd2ec6dbde872f540eb98e48
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-01-30 22:55:58 +0200

    notifications added, and thecontact form updated

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,26 +1,90 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
 import classes from './contact-form.module.css'
+import Notification from '../ui/notification';
+
+async function sendContactData(contactDetails) {
+    const response = await fetch('/api/contact', {
+        method: 'POST',
+        body: JSON.stringify(contactDetails),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+    });
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.message || 'Something went wrong')
+    }
+
+}
+
 
 function ContactForm() {
     const [enteredEmail, setEnteredEmail] = useState('');
     const [enteredName, setEnteredName] = useState('');
     const [enteredMessage, setEnteredMessage] = useState('');
+    const [requestStatus, setRequestStatus] = useState();//'pending', 'success', 'error'
+    const [requestError, setRequestError] = useState();
 
-    function sendMessageHandler(event) {
+    useEffect(()=> {
+        if(requestStatus === 'success' || requestStatus === 'error'){
+         const timer =  setTimeout(()=> {
+                setRequestStatus(null)
+                setRequestError(null)
+            }, 3000);
+            return () => clearTimeout(timer)
+        }
+    },[requestStatus] )
+
+    async function sendMessageHandler(event) {
         event.preventDefault();
 
-        fetch('/api/contact', {
-            method: 'POST',
-            body: JSON.stringify({
+        //I can add more clien-side validation
+
+        setRequestStatus('pending');
+
+        try {
+            await sendContactData({
                 email: enteredEmail,
                 name: enteredName,
                 message: enteredMessage
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            },
+            })
+            setRequestStatus('success');
+            setEnteredMessage('');
+            setEnteredName('');
+            setEnteredEmail('');
+        } catch (error) {
+            setRequestError(error.message);
+            setRequestStatus('error');
+        }
 
-        });
+    }
+
+    let notification;
+    if (requestStatus === 'pending') {
+        notification = {
+            status: 'pending',
+            title: 'Sending message...',
+            message: 'Your message is on it\'s way'
+        }
+    }
+
+    if (requestStatus === 'success') {
+        notification = {
+            status: 'success',
+            title: 'Success',
+            message: 'Message was send successfully'
+        }
+    }
+
+    if (requestStatus === 'error') {
+        notification = {
+            status: 'error',
+            title: 'Error',
+            message: requestError
+        }
     }
 
     return <section className={classes.contact}>
@@ -47,18 +111,25 @@ function ContactForm() {
             </div>
             <div className={classes.control}>
                 <label htmlFor='message'>Your message</label>
-                <textarea 
-                 id='message' 
-                 rows='5'
-                 required
-                 value={enteredMessage}
-                 onChange={event => setEnteredMessage(event.target.value)}
-                 ></textarea>
+                <textarea
+                    id='message'
+                    rows='5'
+                    required
+                    value={enteredMessage}
+                    onChange={event => setEnteredMessage(event.target.value)}
+                ></textarea>
             </div>
             <div className={classes.actions}>
                 <button  >Send message</button>
             </div>
         </form>
+        {notification && (
+            <Notification
+                status={notification.status}
+                title={notification.title}
+                message={notification.message}
+            />
+        )}
     </section>
 }
 
