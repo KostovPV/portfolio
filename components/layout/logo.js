dc95d136fadc295ff8c05e75adcba4992d8cbd7b
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-11-04 11:48:13 +0200

    Header edited

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,7 +1,26 @@
-import clsses from './logo.module.css'
+// import clsses from './logo.module.css'
 
-function Logo(){
-    return <div className={clsses.logo}>Plamen's web page</div>
+// function Logo(){
+//     return <div className={clsses.logo}>Plamen's web page</div>
+// }
+
+// export default Logo
+
+
+import Image from 'next/image';
+import classes from './logo.module.css';
+
+function Logo() {
+    return (
+        <div className={classes.logo}>
+            <Image
+                src="/logo.png" 
+                alt="Plamen's Web Page Logo"
+                width={150}  
+                height={150} 
+            />
+        </div>
+    );
 }
 
-export default Logo
+export default Logo;
